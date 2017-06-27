a6239fa1e9594bb80591490f20bc9632a562fe7a
Frederic Bevilacqua <frederic.bevilacqua@ircam.fr>
2017-06-27 16:11:40 +0200

    corrected name

--- src/client/shared/config.js
+++ src/client/shared/config.js
@@ -3,8 +3,8 @@ const sounds = {
   vent: 'sounds/vent.mp3',
   nage: 'sounds/nage.mp3',
   pierres: 'sounds/pierres.mp3',
-  sable: 'sounds/buble.mp3',
-  feu: 'sounds/oiseaux.mp3',
+  buble: 'sounds/buble.mp3',
+  oiseaux: 'sounds/oiseaux.mp3',
   silence: 'sounds/silence.mp3',
 };
 
