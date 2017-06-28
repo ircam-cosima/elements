a4067d0b48cf697286832f9ef36ce68c8b152287
Joseph <joseph.larralde@gmail.com>
2017-06-28 17:54:20 +0100

    added sounds

--- src/client/shared/config.js
+++ src/client/shared/config.js
@@ -1,10 +1,17 @@
 // collection of { label: soundFile } pairs
 const sounds = {
-  vent: 'sounds/vent.mp3',
-  nage: 'sounds/nage.mp3',
-  pierres: 'sounds/pierres.mp3',
+  aerial: 'sounds/aerial.mp3',
+  birds: 'sounds/oiseaux.mp3',
   buble: 'sounds/buble.mp3',
-  oiseaux: 'sounds/oiseaux.mp3',
+  clicks: 'sounds/clicks.mp3',
+  can: 'sounds/crashedcan.mp3',
+  fire: 'sounds/feu.mp3',
+  noise: 'sounds/noisystroke.mp3',
+  sand: 'sounds/sable.mp3',
+  shake: 'sounds/shake.mp3',
+  stone: 'sounds/pierres.mp3',
+  water: 'sounds/nage.mp3',
+  wind: 'sounds/vent.mp3',
   silence: 'sounds/silence.mp3',
 };
 
