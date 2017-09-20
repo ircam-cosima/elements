1ebd06ab93c7d7a8cfb073e710fb4c432dae45e6
Joseph <joseph.larralde@gmail.com>
2017-09-20 11:31:45 +0200

    fixed multiple start before stop in AutoMotionTrigger + cleaning

--- src/server/shared/appStore.js
+++ src/server/shared/appStore.js
@@ -182,7 +182,6 @@ const appStore = {
 
   // xmm data
   setProjectTrainingData(project, trainingData) {
-    console.log(trainingData);
     xmmDbMapper.persistConfig(project, trainingData.config);
     xmmDbMapper.persistTrainingSet(project, trainingData.trainingSet);
 
