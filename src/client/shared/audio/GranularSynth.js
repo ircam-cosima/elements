f049cad30fd9676aa0372f3f5032a884aa09c062
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2018-01-19 11:22:57 +0100

    added fade on likeliest granular

--- src/client/shared/audio/GranularSynth.js
+++ src/client/shared/audio/GranularSynth.js
@@ -3,16 +3,33 @@ import * as audio from 'waves-audio';
 const audioContext = audio.audioContext;
 const scheduler = audio.getScheduler();
 
-class ProbabilisticGranularSynth {
+
+class GranularSynth {
   constructor() {
     this.buffers = null;
+    this.fadeDuration = 1;
 
     this.output = audioContext.createGain();
     this.output.gain.value = 1;
     this.output.gain.setValueAtTime(1, audioContext.currentTime);
 
-    this.engine = new audio.GranularEngine();
-    this.engine.connect(this.output);
+    this.engines = [];
+
+    const now = audioContext.currentTime;
+    // we need to swap between 2 granular engine to create the cross fade
+    for (let i = 0; i < 2; i++) {
+      const env = new audioContext.createGain();
+      env.connect(this.output);
+      env.gain.value = 1;
+      env.gain.setValueAtTime(0, now);
+
+      const granular = new audio.GranularEngine();
+      granular.connect(env);
+
+      this.engines[i] = { granular, env };
+    }
+
+    this.currentEngineIndex = 0;
   }
 
   set gain(value) {
@@ -25,35 +42,74 @@ class ProbabilisticGranularSynth {
   }
 
   disconnect() {
-    if (this.engine.master)
-      scheduler.remove(this.engine);
+    this.engines.forEach(engine => {
+      if (engine.master)
+        scheduler.remove(engine);
+    });
 
     this.output.disconnect();
   }
 
   trigger(label, bufferIndex) {
+    const now = audioContext.currentTime;
+    // fade out current engine
+    const currentEngineIndex = this.currentEngineIndex;
+    const currentEngine = this.engines[currentEngineIndex];
+
+    if (currentEngine.granular.master) {
+      currentEngine.env.gain.cancelScehduledValues();
+      currentEngine.env.gain.setValueAtTime(currentEngine.env.value, now);
+      currentEngine.env.gain.linearRampToValueAtTime(0, now + this.fadeDuration);
+
+      setTimeout(() => {
+        // if engine has not been readded since last stop, remove from scheduler
+        if (this.currentEngineIndex !== currentEngineIndex && currentEngine.granular.master)
+          scheduler.remove(currentEngine.granular);
+      }, this.fadeDuration);
+    }
+
+    // fade in new engine
+    this.currentEngineIndex = (this.currentEngineIndex + 1) % 2;
+
     const buffer = this.buffers[label][bufferIndex];
     const duration = buffer.duration;
+    const newEngine = this.engines[this.currentEngineIndex];
 
+    newEngine.env.gain.cancelScehduledValues();
+    newEngine.env.gain.setValueAtTime(newEngine.env.gain.value, now);
+    newEngine.env.gain.linearRampToValueAtTime(1, now + this.fadeDuration);
+    newEngine.granular.buffer = buffer;
     // play grain randomly between 1/4 and 3/4 of the buffer
-    this.engine.buffer = buffer;
-    this.engine.position = duration / 2;
-    this.engine.positionVar = duration / 4;
+    newEngine.granular.position = duration / 2;
+    newEngine.granular.positionVar = duration / 4;
 
-    if (!this.engine.master)
-      scheduler.add(this.engine);
+    if (!newEngine.granular.master)
+      scheduler.add(newEngine.granular);
   }
 
   stop(callback = null) {
-    if (this.engine.master)
-      scheduler.remove(this.engine);
+    const currentEngineIndex = this.currentEngineIndex;
+    const currentEngine = this.engines[currentEngineIndex];
+    // engine in scheduler
+    if (currentEngine.granular.master) {
+      const { granular, env } = currentEngine;
+      const now = audioContext.currentTime;
+      env.gain.cancelScehduledValues();
+      env.gain.setValueAtTime(env.gain.value, now);
+      env.gain.linearRampToValueAtTime(0, now + this.fadeDuration);
 
-    if (callback)
+      setTimeout(() => {
+        scheduler.remove(currentEngine);
+
+        if (callback)
+          callback();
+      }, this.fadeDuration);
+    } else if (callback !== null) {
       callback();
+    }
   }
-
 }
 
-export default ProbabilisticGranularSynth;
+export default GranularSynth;
 
 
